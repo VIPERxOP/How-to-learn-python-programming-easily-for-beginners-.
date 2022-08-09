e686f06a6658a18d36b3227f0acdc0e22735ba2e
VIPERxOP <109715946+VIPERxOP@users.noreply.github.com>
2022-08-09 01:49:53 -0700

    python learning file
    learn and enjoy

---
+++ main.py
@@ -0,0 +1,14 @@
+#this is the first program which print hello world
+print("Hello World!")
+
+#this is a integer in the parantesis
+print(25)
+
+
+print( 25, 22)
+
+#this inside the qoutes are the string 
+print("My name is Aryan")
+
+#how to print multi string 
+print("Hello", "World")
